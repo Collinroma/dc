7deb402428935889c2f687c471d830c1dbb1e65c
Collinroma <68510255+Collinroma@users.noreply.github.com>
2023-11-21 11:35:22 -0500

    Add files via upload

---
+++ drive_care/info.py
@@ -0,0 +1,8 @@
+import os
+# email configs
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = str('andrewbola112@gmail.com')
+EMAIL_HOST_PASSWORD = str('grjz cvuu qvlv vexo')
